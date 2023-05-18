cfb3fca9d6d68da637d5242b182786c9e20666ef
joedonovan <joepeterdonovan@gmail.com>
2023-05-18 12:18:25 -0500

    redid the audio recorder to record audio to wav files

--- jist/src/App.js
+++ jist/src/App.js
@@ -3,33 +3,23 @@ import axios from "axios";
 import AudioRecorder from "./AudioRecorder";
 
 export default function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000");
-        setData(response.data);
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("http://localhost:8000");
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error("Error: ", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="audio-recorder">
       <AudioRecorder />
-      <div className="textbox">
-        {data ? (
-          <ul>
-            {data.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        ): (
-          <p>...Loading...</p>
-        )}
-      </div>
     </div>
   );
 }
--- jist/src/AudioRecorder.js
+++ jist/src/AudioRecorder.js
@@ -1,67 +1,45 @@
-import React from 'react'; 
-import { useState } from 'react'; 
-import { ReactMic } from 'react-mic';
-import axios from 'axios';
+import React, { useRef} from "react";
+import {ReactMediaRecorder} from "react-media-recorder";
+import axios from "axios";
 
-export default function AudioRecorder (){
-
-const [isRecording, setIsRecording] = useState(false);
-  const [blobURL, setBlobURL] = useState(null);
-
-  const startRecording = () => {
-    setIsRecording(true);
-  }
-  const stopRecording = () =>{
-    setIsRecording(false);
-    sendAudioToBackend(); 
-  }
-
-  const onData = (recordedBlob) => {
-    console.log('chunk of real-time data is: ', recordedBlob);
-  };
-
-  const onStop = (recordedBlob) => {
-    console.log("recordedBlob is: ", recordedBlob);
-    setBlobURL(recordedBlob.blobURL); 
-  }
- 
-  const sendAudioToBackend = async () =>{
-    try{
-      const audioBlob = new Blob([setBlobURL], { type: 'audio.webm'});
-      const formData = new FormData();
-      formData.append('audio', audioBlob)
-      formData.append("Content-Type", 'audio/webm')
-    
-      const response = await axios.post('http://localhost:8000/', formData)
-      console.log(response.data); 
-      console.log(audioBlob)
-      } catch (error){
-        console.error(error)
-      } 
- }
-
+const AudioRecorder = () => {
+  const mediaBlobUrl = useRef(null)
   
-  return(
-      <div className='App'>
-        <h1>Audio Recorder</h1>
-        {blobURL && (
-          <audio controls>
-            <source src={blobURL} type='audio.webM'/>
-          </audio>
-        )}
-        <ReactMic
-          record={isRecording}
-          onStop={onStop}
-          ondata={onData}
-          strokeColor='black'
-          backgroundColor='white'
-          mimeType='audio.webM'
-        />
-        {isRecording ? (
-          <button onClick={stopRecording}>Stop Recording</button>
-        ) : (
+  const handleRecordingStop = async (blob) => {
+    //send the recorded audio to django api
+    const file = new File([blob], 'audio.wav', {
+      type: 'audio/wav',
+    })
+    const formData = new FormData();
+    formData.append("audio", file);
+
+    try {
+      const response = await axios.post("http://localhost:8000", formData) 
+      if (response.status === 200) {
+        console.log("Audio sent successfully");
+      } else {
+        console.error("Error sending audio");
+      }
+    } catch (error) {
+      console.error("Error sending audio", error);
+    }
+  };
+  return (
+    <ReactMediaRecorder
+      video={false}
+      render={({ startRecording, stopRecording, mediaBlobUrl }) => (
+        //Saving media blob
+       
+         <div>
           <button onClick={startRecording}>Start Recording</button>
-          )}
-      </div>
-  )
-}
+          <button onClick={stopRecording}>Stop Recording</button>
+          <audio src={mediaBlobUrl} controls />
+        </div>
+        
+    )}
+      onStop={handleRecordingStop}
+    />
+  );
+};
+
+export default AudioRecorder
