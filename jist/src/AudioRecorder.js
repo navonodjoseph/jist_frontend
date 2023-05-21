8321119655f1eb4844ac7d7a2791ba189d45e3ae
joedonovan <joepeterdonovan@gmail.com>
2023-05-21 09:37:44 -0500

    accurately prints out blob object

--- jist/src/AudioRecorder.js
+++ jist/src/AudioRecorder.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 import axios from "axios";
 import { Button, Box} from '@chakra-ui/react'
 
 const AudioRecorder = () => {
   const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ audio: true });
+    useReactMediaRecorder({ audio: true, video: false });
+  const [audioBlob, setAudioBlob] = useState(null); 
+
+  useEffect(()=> {
+    if (mediaBlobUrl){
+      fetch(mediaBlobUrl)
+      .then((response) => response.blob())
+      .then((blob) => setAudioBlob(blob))
+      .catch((error) => console.error('Error fetching audio Blob ln 16', error))
+    }
+
+  }, [mediaBlobUrl])
 
   const handleSaveRecording = async () => {
     //stop recording if in progress
@@ -15,7 +26,8 @@ const AudioRecorder = () => {
     // create a formData object to send the recorded audio file
 
     const formData = new FormData();
-    formData.append("audio", mediaBlobUrl);
+    formData.append("audio", audioBlob, 'audio.wav');
+    console.log('audio ln 30', audioBlob)
 
     try {
       //send audio to backend
